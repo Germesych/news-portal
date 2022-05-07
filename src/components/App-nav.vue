c0501c45f3d404bc52f08e98da5b703ff38a6ada
germesych <kaiot1984@google.com>
2022-05-07 06:02:49 +0300

    create search-page

--- src/components/App-nav.vue
+++ src/components/App-nav.vue
@@ -7,6 +7,18 @@
         </li>
         <!-- /.nav-items-->
         <li class="nav-items">
+          <router-link to="/blog">blog</router-link>
+        </li>
+        <!-- /.nav-items -->
+        <li class="nav-items">
+          <router-link to="/article">article</router-link>
+        </li>
+        <!-- /.nav-items -->
+        <li class="nav-items">
+          <router-link to="/search">search</router-link>
+        </li>
+        <!-- /.nav-items -->
+        <li class="nav-items">
           <router-link to="/business">BUSINESS</router-link>
         </li>
         <!-- /.nav-items-->
@@ -20,18 +32,6 @@
         <!-- /.nav-items -->
         <li class="nav-items">
           <router-link to="/food">FOODS</router-link>
-        </li>
-        <!-- /.nav-items -->
-        <li class="nav-items">
-          <router-link to="/culture">CULTURE</router-link>
-        </li>
-        <!-- /.nav-items -->
-        <li class="nav-items">
-          <router-link to="/tech">TECH</router-link>
-        </li>
-        <!-- /.nav-items -->
-        <li class="nav-items">
-          <router-link to="/sport">SPORT</router-link>
         </li>
         <!-- /.nav-items -->
         <li class="nav-items">
--- src/views/Search-page.vue
+++ src/views/Search-page.vue
@@ -1,10 +1,147 @@
 <template>
-  <h1>Search Page</h1>
+  <div class="search-page">
+    <div class="flex-block">
+      <div class="search-block">
+        <h2 class="title">Search Results</h2>
+        <form class="search">
+          <input type="text" placeholder="Search">
+          <button class="btn-search" type="submit">
+            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M6.60879 12.605L6.60121 11.8151L6.86851 11.5325C8.01786 12.5015 9.50346 13.0773 11.1134 13.0618C14.7032 13.0273 17.5851 10.0895 17.5507 6.4997C17.5162 2.90987 14.5784 0.0279368 10.9886 0.0624018C7.39875 0.0968668 4.51682 3.03467 4.55128 6.6245C4.56674 8.23443 5.17092 9.7087 6.16182 10.8392L5.88442 11.1119L5.09446 11.1195L0.152692 16.1672L1.65693 17.6428L6.60879 12.605ZM6.55119 6.6053C6.52729 4.11542 8.5179 2.08621 11.0078 2.06231C13.4977 2.0384 15.5269 4.02902 15.5508 6.5189C15.5747 9.00879 13.5841 11.038 11.0942 11.0619C8.6043 11.0858 6.57509 9.09519 6.55119 6.6053Z" fill="#F44336"/>
+            </svg>
+          </button>
+        </form>
+        <!-- /.search -->
+        <div class="search-info">
+          Showing <span>1-10</span> of <span>675</span> for
+        </div>
+        <!-- /.search-info -->
+        <div class="search-result">
+          <div class="row">
+            <h1 class="title">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit nulla lorem aliquam sem amet, leo sed. Non ac aliquet viverra pellentesque varius ac eleifend varius amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <!-- /.row -->
+          <div class="row">
+            <h1 class="title">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit nulla lorem aliquam sem amet, leo sed. Non ac aliquet viverra pellentesque varius ac eleifend varius amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <!-- /.row -->
+          <div class="row">
+            <h1 class="title">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit nulla lorem aliquam sem amet, leo sed. Non ac aliquet viverra pellentesque varius ac eleifend varius amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <!-- /.row -->
+          <div class="row">
+            <h1 class="title">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit nulla lorem aliquam sem amet, leo sed. Non ac aliquet viverra pellentesque varius ac eleifend varius amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <!-- /.row -->
+          <div class="row">
+            <h1 class="title">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit nulla lorem aliquam sem amet, leo sed. Non ac aliquet viverra pellentesque varius ac eleifend varius amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <!-- /.row -->
+          <div class="row">
+            <h1 class="title">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit nulla lorem aliquam sem amet, leo sed. Non ac aliquet viverra pellentesque varius ac eleifend varius amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <!-- /.row -->
+          <div class="row">
+            <h1 class="title">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit nulla lorem aliquam sem amet, leo sed. Non ac aliquet viverra pellentesque varius ac eleifend varius amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <!-- /.row -->
+          <div class="row">
+            <h1 class="title">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit nulla lorem aliquam sem amet, leo sed. Non ac aliquet viverra pellentesque varius ac eleifend varius amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <!-- /.row -->
+          <div class="row">
+            <h1 class="title">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit nulla lorem aliquam sem amet, leo sed. Non ac aliquet viverra pellentesque varius ac eleifend varius amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <!-- /.row -->
+          <div class="row">
+            <h1 class="title">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit nulla lorem aliquam sem amet, leo sed. Non ac aliquet viverra pellentesque varius ac eleifend varius amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.search-result -->
+        <div class="pagination">
+          <button class="pagination-prev btn-disable">&lsaquo;</button>
+          <a href="" class="pagination-link pagination-link__active">1</a>
+          <!-- /.pagination-link -->
+          <a href="" class="pagination-link">2</a>
+          <!-- /.pagination-link -->
+          <a href="" class="pagination-link">3</a>
+          <!-- /.pagination-link -->
+          <a href="" class="pagination-link">4</a>
+          <!-- /.pagination-link -->
+          <a href="" class="pagination-link">5</a>
+          <!-- /.pagination-link -->
+          <button class="pagination-next">&rsaquo;</button>
+        </div>
+        <!-- /.pagination -->
+      </div>
+      <!-- /.search-block -->
+      <aside class="aside">
+        <app-adblock/>
+        <app-adblock/>
+      </aside>
+      <!-- /.aside -->
+    </div>
+    <!-- /.flex-block -->
+  </div>
+  <!-- /.search-page -->
 </template>
 
 <script>
+import AppAdblock from "../components/App-adblock";
 export default {
-  name: "Search-page"
+  name: "Search-page",
+  components: {AppAdblock}
 }
 </script>
 
